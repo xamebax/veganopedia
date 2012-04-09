f6b726f2b3e38a5c725f80225910957fa6cd099b
Jakub Matraszek <jakub.matraszek@gmail.com>
2012-04-09 22:53:29 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veganopedia::Application.config.secret_token = '196b76f6fd6711327fb0152ef78c0edbf8c1cf420903ddf6bb4d099ae6956289588a50960eb0aa7ac275802451a3445186594f8da3effd4807d9ae6c0b0bdf41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Veganopedia::Application.config.session_store :cookie_store, key: '_veganopedia_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Veganopedia::Application.config.session_store :active_record_store
