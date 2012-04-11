9a09177eb513709d5f2520d87ada55354dccb6e5
Jakub Matraszek <jakub.matraszek@gmail.com>
2012-04-12 00:12:43 +0200

    allow users to login with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,50 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :confirmable, :database_authenticatable, :encryptable, :omniauthable,
          :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
+  end
+
+  def self.send_reset_password_instructions(attributes={})
+    recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    recoverable.send_reset_password_instructions if recoverable.persisted?
+    recoverable
+  end
+
+  def self.find_recoverable_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
+    (case_insensitive_keys || []).each { |k| attributes[k].try(:downcase!) }
+
+    attributes = attributes.slice(*required_attributes)
+    attributes.delete_if { |key, value| value.blank? }
+
+    if attributes.size == required_attributes.size
+      if attributes.has_key?(:login)
+        login = attributes[:login]
+        record = find_record(login)
+      else
+        record = where(attributes).first
+      end
+    end
+
+    unless record
+      record = new
+
+      required_attributes.each do |key|
+        value = attributes[key]
+        record.send("#{key}=", value)
+        record.errors.add(key, value.present? ? error : :blank)
+      end
+    end
+    record
+  end
+
+  def self.find_record(login)
+    where(["username = :value OR email = :value", { :value => login }]).first
+  end
 end
--- db/migrate/20120409224231_devise_create_users.rb
+++ db/migrate/20120409224231_devise_create_users.rb
@@ -36,11 +36,13 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Token authenticatable
       # t.string :authentication_token
 
+      t.string  :username, :null => false, :default => '', :limit => 16
 
       t.timestamps
     end
 
     add_index :users, :email,                :unique => true
+    # add_index :users, :username,             :unigue => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
