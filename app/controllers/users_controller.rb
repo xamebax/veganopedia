1c526ca22026ccc63b3be28a4b41859bf8fd9c3f
Jakub Matraszek <jakub.matraszek@gmail.com>
2012-04-18 23:48:03 +0200

    generated twt-bootstrap views for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,31 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    respond_with @user
   end
 
   def new
+    @user = User.new
+    respond_with @user
   end
 
   def create
+
   end
 
   def edit
+    @user = User.find(params[:id])
+    respond_with @user
   end
 
   def update
+
+    redirect_to users_path, :notice => 'Successfully deleted user.'
   end
 
   def destroy
+    User.find(params[:id])
+    redirect_to users_path, :notice => 'Successfully deleted user.'
   end
 end
