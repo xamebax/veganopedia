6cf1e2c486c4ec4311b8c8646351a0c8edeaecc8
Jakub Matraszek <jakub.matraszek@gmail.com>
2012-04-20 00:16:26 +0200

    added navigasmic gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Veganopedia::Application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
   # root :to => 'devise/registrations#new'
   devise_scope :user do
     root :to => 'users#index'
