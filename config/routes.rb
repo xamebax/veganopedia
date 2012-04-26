70633b2d4018094c9e910e9313c3c4794df5aa23
Jakub Matraszek <jakubm@innov8on.com>
2012-04-26 15:11:29 +0200

    removed deprecation warning from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Veganopedia::Application.routes.draw do
-  devise_for :users do
-    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
-  end
+  devise_for :users
   # root :to => 'devise/registrations#new'
   devise_scope :user do
     root :to => 'users#index'
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
   end
   resources :users, :only => [:index, :show, :edit, :update, :destroy]
   resources :products, :only => [:index, :show, :new, :create, :edit, :update, :destroy]
