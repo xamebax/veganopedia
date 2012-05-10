99106f3eb685f768d7f99ae51305b8b61d839e42
Jakub Matraszek <jakubm@innov8on.com>
2012-05-10 17:04:06 +0200

    added genders to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+connection = ActiveRecord::Base.connection()
+connection.execute("INSERT INTO genders (name) VALUES ('Female');")
+connection.execute("INSERT INTO genders (name) VALUES ('Male');")
+connection.execute("INSERT INTO genders (name) VALUES ('Other');")
